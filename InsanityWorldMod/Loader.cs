599823921990f2cd82fe12872c2c1b6b02956292
lapkadev <lapkaway@gmail.com>
2026-05-30 22:34:34 +0300

    Initial commit

---
+++ InsanityWorldMod/Loader.cs
@@ -0,0 +1,13 @@
+namespace InsanityWorldMod
+{
+	public class Loader
+	{
+		/// <summary>
+		/// This method is run by Winch to initialize your mod
+		/// </summary>
+		public static void Initialize()
+		{
+			Api.Bootstrap.Initialize();
+		}
+	}
+}
