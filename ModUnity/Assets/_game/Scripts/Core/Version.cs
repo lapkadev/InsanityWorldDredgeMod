627facb6cc86023309752c6ad306b2865f50f840
lapkadev <lapkaway@gmail.com>
2026-06-03 16:19:33 +0300

    v0.2.1

--- ModUnity/Assets/_game/Scripts/Core/Version.cs
+++ ModUnity/Assets/_game/Scripts/Core/Version.cs
@@ -14,6 +14,6 @@ namespace InsanityWorldMod.Core
     /// </summary>
     public static class ModVersion
     {
-        public const string Current = "0.2.0";
+        public const string Current = "0.2.1";
     }
 }
